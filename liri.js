3d6f6c617794b47769e7198c0aea7ec08edd84d9
Frank Flores <frankthetank67@gmail.com>
2020-04-18 19:11:43 -0700

    added concertThis function to make api call to BandsInTown for concerts happening, returning venue, location, and date information

--- liri.js
+++ liri.js
@@ -1,5 +1,57 @@
 require("dotenv").config();
-
+const {prompt} = require('inquirer');
 var keys = require("./keys.js");
+var axios = require('axios');
+var Spotify = require('node-spotify-api');
+var fs = require('fs');
+var moment = require('moment');
 
 var spotify = new Spotify(keys.spotify);
+
+const initPrompt = {
+    message: "welcome to liri, what would you like to do?",
+    name: "choices",
+    type: 'list',
+    choices: ['concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says']
+}
+
+async function init(){
+    const {choices} = await prompt(initPrompt);
+    switch (choices){
+        case 'concert-this':
+            concertThis();
+            break;
+        case 'spotify-this-song':
+            spotifyThis();
+            break;
+        case 'movie-this':
+            movieThis();
+            break;
+        case 'do-what-it-says':
+            doWhatItSays();
+            break;
+    }
+}
+
+async function concertThis(){
+    const {band} = await prompt({message: "Please enter artist/band name", name: 'band'});
+    //api call here
+    const {data} = await axios.get(`https://rest.bandsintown.com/artists/${band}/events/?app_id=codingbootcamp`);
+
+    for (var i = 0; i < data.length; i++) {
+        var venue = data[i].venue.name;
+        var location = data[i].venue.location;
+        var date = data[i].datetime;
+
+
+
+        console.log(`\nVenue: ${venue}\nLocation: ${location}\nDate: ${moment(date).format("MM/DD/YYYY")}\n`);
+    }
+    
+}
+
+async function spotifyThis() {
+    const{song} = await prompt({message: "Please enter song title", name: 'song'})
+}
+
+init();
